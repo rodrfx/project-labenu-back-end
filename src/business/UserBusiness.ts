e5561c0a423a94b7a82cddd6ca21b1109f92aa6b
Rodrigo Xavier <rodrfx@gmail.com>
2021-03-14 10:52:03 -0300

    user-business

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -1,6 +1,29 @@
+import { UserDatabase } from "../data/UserDatabase";
+import { HashManager } from "../services/HashManger";
+import { IdGenerator } from "../services/idGenerator";
+import { TokenManager } from "../services/TokenManagar";
+
 export class UserBusiness {
-    async create(){
+  async create(
+    name: string,
+    email: string,
+    password: string,
+    nickname: string
+  ):Promise <string> {
+    try {
+      const idGenerator = new IdGenerator();
+      const hashManager = new HashManager();
+      const tokenManager = new TokenManager();
+      const userDatabase = new UserDatabase();
 
+      const id = idGenerator.generate();
+      const hashPassword = await hashManager.hash(password);
+      await userDatabase.create(id, name, email, hashPassword, nickname);
 
-        }
+      const token = tokenManager.generate(id);
+      return token;
+    } catch (error) {
+      throw new Error("Erro ao criar " + error.message);
     }
+  }
+}
